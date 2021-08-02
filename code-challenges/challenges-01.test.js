4a72fa1e6bd5d38073587a9d62a297fb316d6d7a
emanmkhareez <emkhareez19@gmail.com>
2021-08-02 01:01:09 -0700

    add

--- code-challenges/challenges-01.test.js
+++ code-challenges/challenges-01.test.js
@@ -1,6 +1,45 @@
 'use strict';
 
-/* ------------------------------------------------------------------------------------------------
+/* -----------------------------------------------------------------------------------------------
+// Array.forEach is a method on an array that processes every element in the array with a callback
+// The callback is always given the current element's value and index in the array as args
+
+let people = ['Razan','Sherry','Abdulrahman','Aisha'];
+// for (let i=0;i<people.length;i++) {
+//   console.log(people[i]);
+// }
+
+// people.forEach(function(person,index){
+//   console.log(person);
+//   console.log(index);
+// })
+
+// console.log('-----------')
+
+// people.forEach(function(index,person){
+//   console.log(person);
+//   console.log(index);
+// })
+
+// people.forEach((item,index)=>{
+//   console.log(item);
+//   console.log(index);
+// })
+
+
+function displayName(item,index) {
+  console.log(item);
+  // console.log(people[index]);
+  console.log(item[index]);
+}
+
+// let displayName = (item,index) => {
+//   console.log(item);
+// }
+people.forEach(displayName);-
+
+
+
 CHALLENGE 1
 
 Write a function named `addOne` that takes an array of numbers, and returns a new array of the numbers, incremented by 1.
