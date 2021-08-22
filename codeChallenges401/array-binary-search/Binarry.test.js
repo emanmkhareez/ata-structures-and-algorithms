2d919f469e5684c48daf220bc4da00bfc203bbd6
emanmkhareez <emkhareez19@gmail.com>
2021-08-21 22:47:51 -0700

    binarytest 😍😍

--- codeChallenges401/array-binary-search/Binarry.test.js
+++ codeChallenges401/array-binary-search/Binarry.test.js
@@ -1,10 +1,10 @@
 'use strict'
 
-      const binarySearch=require('./binarySearch')
+      const BinarrySearch=require('./BinarrySearch')
       
       describe('Testing challenge 3', () => {
         it('It returns an index to item  ', () => {
-            expect(binarySearch([-131, -82, 0],-82)).toStrictEqual(1);
-            expect(binarySearch([-131, -82, 0],6)).toStrictEqual(-1);
+            expect(BinarrySearch([-131, -82, 0],-82)).toStrictEqual(1);
+            expect(BinarrySearch([-131, -82, 0],6)).toStrictEqual(-1);
         });
     });
