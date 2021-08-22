07225f112a06d4c44f7bf542e1dd5b61b3878e4d
emanmkhareez <emkhareez19@gmail.com>
2021-08-21 22:58:30 -0700

    binarytest 😍😍

--- codeChallenges401/array-binary-search/Binarry.test.js
+++ codeChallenges401/array-binary-search/Binarry.test.js
@@ -4,7 +4,7 @@
       
       describe('Testing challenge 3', () => {
         it('It returns an index to item  ', () => {
-            expect(BinarrySearch([-131, -82, 0],-82)).toStrictEqual(1);
-            expect(BinarrySearch([-131, -82, 0],6)).toStrictEqual(-1);
+            expect(BinarrySearch([-131, -82, 0],-82)).toBe(1);
+            expect(BinarrySearch([-131, -82, 0],6)).toBe(-1);
         });
     });
